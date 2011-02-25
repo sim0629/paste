74b6e78972e09722ada9d04ee39bc4091a12a880
Roberto Rodriguez <rodpin@gmail.com>
2011-02-25 19:24:42 -0300

    I added a variable with the kind of database used between postgres y mysql. The copyright notice has been updated.

--- classes/config.inc.php
+++ classes/config.inc.php
@@ -1,7 +1,8 @@
 <?php
 /*
-* $ID PROJECT: Paste - config.inc.php, v1 EcKstasy - 17/03/2010/06:30 GMT+1 (dd/mm/yy/time) 
-* 
+* Copyright (C) 2010 EcKstasy <eckstasy@escriptirc.com>
+*               2011 Roberto Rodríguez Pino <rodpin@gmail.com>
+*
 * This program is free software; you can redistribute it and/or
 * modify it under the terms of the GNU General Public License
 * as published by the Free Software Foundation; either version 3
@@ -11,6 +12,10 @@
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 * GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*
 */
  
 $CONF=array();
@@ -18,6 +23,19 @@ $CONF=array();
 // Include the configration file
 require_once('config.php');
 
+//Set the database to use: postgresql, mysql
+$CONF['database']='postgresql';
+
+//path for the pastebin => for http:/domain/pastebin/
+$CONF['pastebin']='/pastebin';
+
 // Pull in the required database class.
-require_once('classes/mysql.php');
+switch($CONF['database']){
+    case "postgresql":
+        require_once('classes/postgresql.php');
+        break;
+    case "mysql":
+        require_once('classes/mysql.php');
+        break;
+}
 ?>
